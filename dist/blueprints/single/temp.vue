3924b9517ce1cce39eacf7015d0b795372e100eb
Ian Egner <ian.egner@apc-overnight.com>
2016-12-01 10:25:09 +0000

    Add dynamic lang attribute single file template

--- dist/blueprints/single/temp.vue
+++ dist/blueprints/single/temp.vue
@@ -1,23 +1,23 @@
-<template>
+<template lang="{{filesType.html}}">
   <section class="{{name | kebabCase}}">
     <h1>{{name | kebabCase}} Component</h1>
   </section>
 </template>
 
-<script>
+<script lang="{{ filesType.js }}">
   export default  {
     name: '{{name | kebabCase}}',
     props: [],
     mounted() {
-      
+
     },
     data() {
       return {
-        
+
       }
     },
     methods: {
-    
+
     },
     computed: {
 
@@ -25,7 +25,7 @@
 }
 </script>
 
-<style scoped>
+<style scoped lang="{{ filesType.style }}">
   .{{name | kebabCase}} {
 
   }
