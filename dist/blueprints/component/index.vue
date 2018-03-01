a04fab04d90e46b5a2c6b00de0f79a28d461d63a
Mark Crisp <macr1324@gmail.com>
2018-02-28 19:40:39 -0500

    updated dist

--- dist/blueprints/component/index.vue
+++ dist/blueprints/component/index.vue
@@ -1,5 +1,4 @@
 <template src="./{{name}}.component.{{ filesType.html }}"></template>
-<script src="./{{name}}.component.{{ filesType.script }}" lang="{{ filesType.script }}"></script>
 <script src="./{{name}}.component.{{ filesType.script }}"{% if filesType.script !="js" %} lang="{{ filesType.script }}"{% endif %}></script>
 <style src="./{{name}}.component.{{ filesType.style }}" scoped lang="{{ filesType.style }}"></style>
 
